3e38b59e853c57c1332afcd5ca824fa94bbfa151
tylerscheuble <tylerscheuble@gmail.com>
2017-02-09 09:37:37 -0800

    Add unfinished xbox controller map

---
+++ src/main/java/org/team2471/frc/lib/io/maps/XboxMap.java
@@ -0,0 +1,28 @@
+package org.team2471.frc.lib.io.maps;
+
+import java.util.function.IntSupplier;
+
+// TODO: update values
+public final class XboxMap {
+  public static final class Buttons {
+    public static final int A = 1;
+    public static final int B = 2;
+    public static final int X = 3;
+    public static final int Y = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int BACK = 7;
+    public static final int START = 8;
+    public static final int LEFT_THUMBSTICK = 9;
+    public static final int RIGHT_THUMBSTICK = 10;
+  }
+
+  public static final class Axises {
+    public static final int LEFT_THUMBSTICK_X = 0;
+    public static final int LEFT_THUMBSTICK_Y = 1;
+    public static final int LEFT_TRIGGER = 2;
+    public static final int RIGHT_TRIGGER = 3;
+    public static final int RIGHT_THUMBSTICK_X = 4;
+    public static final int RIGHT_THUMBSTICK_Y = 5;
+  }
+}
