aef908a0e44dfd2b77a109995e79dc13e3740ff7
tylerscheuble <tylerscheuble@gmail.com>
2017-10-01 17:51:50 -0700

    I made a command system

---
+++ src/main/kotlin/org/team2471/frc/lib/control/command/Command.kt
@@ -0,0 +1,61 @@
+package org.team2471.frc.lib.control.command
+
+import java.util.LinkedList
+
+abstract class Subsystem(internal val defaultCommand: Command? = null)
+
+abstract class Command(internal val requirements: List<Subsystem> = emptyList()) {
+    open fun initialize() = Unit
+
+    open fun execute() = Unit
+
+    abstract fun isFinished(): Boolean
+
+    open fun end() = Unit
+
+    open fun interrupted() = Unit
+}
+
+object Scheduler {
+    private val commands: MutableList<Command> = LinkedList()
+
+    fun runCommand(command: Command){
+        interruptCommand(command)
+
+        commands.add(command)
+        command.initialize()
+    }
+
+    fun interruptCommand(command: Command) {
+        if(command !in commands) return
+
+        command.interrupted()
+        removeCommand(command)
+    }
+
+    fun isRunningCommand(command: Command) = command in commands
+
+    fun tick() {
+        // handle commands
+        for(command in commands) {
+            command.execute()
+            if(command.isFinished()) {
+                command.end()
+                removeCommand(command)
+            }
+        }
+
+        // run triggers
+        triggerFunctions.forEach { it() }
+    }
+
+    private fun removeCommand(command: Command) {
+        if(commands.remove(command)) {
+            command.requirements.forEach { subsystem ->
+                subsystem.defaultCommand?.let { defaultCommand ->
+                    runCommand(defaultCommand)
+                }
+            }
+        }
+    }
+}
