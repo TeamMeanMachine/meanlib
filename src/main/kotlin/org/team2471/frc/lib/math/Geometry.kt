f4aba9d729c02e63ffdbfb8100726ce3cc340184
tylerscheuble <tylerscheuble@gmail.com>
2017-10-05 20:32:12 -0700

    Move Point2D methods to class body

--- src/main/kotlin/org/team2471/frc/lib/math/Geometry.kt
+++ src/main/kotlin/org/team2471/frc/lib/math/Geometry.kt
@@ -1,13 +1,28 @@
 package org.team2471.frc.lib.math
 
-data class Point2D(val x: Double, val y: Double)
+data class Point2D(val x: Double, val y: Double) {
+    companion object {
+        @JvmStatic
+        val origin: Point2D = Point2D(0.0, 0.0)
+    }
 
-operator fun Point2D.unaryMinus() = Point2D(-x, -y)
+    operator fun unaryPlus() = this
 
-operator fun Point2D.plus(b: Point2D) = Point2D(x + b.x, y + b.y)
+    operator fun unaryMinus() = Point2D(-x, -y)
 
-operator fun Point2D.minus(b: Point2D) = Point2D(x - b.x, y - b.y)
+    operator fun plus(b: Point2D) = Point2D(x + b.x, y + b.y)
 
-operator fun Point2D.times(scalar: Double) = Point2D(x * scalar, y * scalar)
+    operator fun plus(vec: Vector2D) = Point2D(x + vec.x, y + vec.y)
 
-operator fun Point2D.div(scalar: Double) = Point2D(x / scalar, y / scalar)
+    operator fun minus(b: Point2D) = Point2D(x - b.x, y - b.y)
+
+    operator fun minus(vec: Vector2D) = Point2D(x - vec.x, y - vec.y)
+
+    operator fun times(scalar: Double) = Point2D(x * scalar, y * scalar)
+
+    operator fun div(scalar: Double) = Point2D(x / scalar, y / scalar)
+
+    fun distance(b: Point2D): Double = Math.sqrt(
+            Math.pow(b.x - this.x, 2.0) + Math.pow(b.y - this.y, 2.0)
+    )
+}
