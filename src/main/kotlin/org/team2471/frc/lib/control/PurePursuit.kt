91c69a115032755158e40b95fbb8732662860cec
tylerscheuble <tylerscheuble@gmail.com>
2017-10-09 01:58:55 -0700

    First attempt at Pure Pursuit

--- src/main/kotlin/org/team2471/frc/lib/control/PurePursuit.kt
+++ src/main/kotlin/org/team2471/frc/lib/control/PurePursuit.kt
@@ -1,13 +1,14 @@
 package org.team2471.frc.lib.control
 
+import org.team2471.frc.lib.math.Circle
+import org.team2471.frc.lib.math.Line2D
 import org.team2471.frc.lib.math.Point2D
 import org.team2471.frc.lib.math.Vector2D
 import java.lang.Math.toDegrees
-import java.util.*
 
 data class Waypoint(val point: Point2D, val radialTolerance: Double)
 
-// see: http://www.ri.cmu.edu/pub_files/pub1/kelly_alonzo_1994_4/kelly_alonzo_1994_4.pdf
+// see: http://www.dtic.mil/get-tr-doc/pdf?AD=ADA599492
 class PurePursuitController(private val lookahead: Double,
                             private var position: Point2D,
                             private var heading: Double,
@@ -15,11 +16,13 @@ class PurePursuitController(private val lookahead: Double,
     var finished = false
         private set
 
-    private val remainingWaypoints: LinkedList<Waypoint> = LinkedList(waypoints.asList())
-    private var lastPoint = position
-    private val currentWaypoint = remainingWaypoints[0]
+    private val waypoints = listOf(Waypoint(position, Double.MAX_VALUE), *waypoints)
+    private var waypointN = 0
 
-    private var lookaheadPoint: Point2D = position
+    private val lastWaypoint get() = waypoints[waypointN]
+    private val currentWaypoint get() = waypoints[waypointN + 1]
+
+    private var lookaheadPoint: Point2D = currentWaypoint.point
 
     private val error: Double
         get() = position.vectorTo(lookaheadPoint).angle
@@ -29,11 +32,22 @@ class PurePursuitController(private val lookahead: Double,
         position += deltaVector
         heading += toDegrees(deltaVector.angle)
 
-
+        // check if current waypoint reached
         if (position.distance(currentWaypoint.point) <= currentWaypoint.radialTolerance) {
-            lastPoint = currentWaypoint.point
+            if (waypointN + 1 < waypoints.size) waypointN++
+            else finished = true
         }
 
+        // update lookahead point
+        val lookaheadPoints = Circle(position, lookahead)
+                .intersectingPoints(Line2D(lastWaypoint.point, currentWaypoint.point))
+
+        lookaheadPoint = when (lookaheadPoints.size) {
+            1 -> lookaheadPoints[0]
+            2 -> if(lookaheadPoints[0].distance(currentWaypoint.point) < lookaheadPoints[1].distance(currentWaypoint.point))
+                lookaheadPoints[0] else lookaheadPoints[1]
+            else -> lookaheadPoint // this should only happen if there is no intersecting points. there should never be more than 2 points.
+        }
 
         return (2 * error) / lookahead
     }
