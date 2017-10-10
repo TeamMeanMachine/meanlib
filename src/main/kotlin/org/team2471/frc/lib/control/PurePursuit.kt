65ec1647f841a2d7d76f10a400a948c673a68e42
tylerscheuble <tylerscheuble@gmail.com>
2017-10-09 21:28:08 -0700

    Use heading in pure pursuit + minor improvements

--- src/main/kotlin/org/team2471/frc/lib/control/PurePursuit.kt
+++ src/main/kotlin/org/team2471/frc/lib/control/PurePursuit.kt
@@ -6,7 +6,10 @@ import org.team2471.frc.lib.math.Point2D
 import org.team2471.frc.lib.math.Vector2D
 import java.lang.Math.toDegrees
 
-data class Waypoint(val point: Point2D, val radialTolerance: Double)
+data class Waypoint(val point: Point2D,
+                    val radialTolerance: Double,
+                    val maxSpeed: Double = Double.MAX_VALUE,
+                    val name: String? = null)
 
 // see: http://www.dtic.mil/get-tr-doc/pdf?AD=ADA599492
 class PurePursuitController(private val lookahead: Double,
@@ -17,15 +20,16 @@ class PurePursuitController(private val lookahead: Double,
         private set
 
     private val waypoints = listOf(Waypoint(position, Double.MAX_VALUE), *waypoints)
-    private var waypointN = 0
+    var waypointsPassed = 0
+        private set
 
-    private val lastWaypoint get() = waypoints[waypointN]
-    private val currentWaypoint get() = waypoints[waypointN + 1]
+    private val lastWaypoint get() = waypoints[waypointsPassed]
+    val currentWaypoint get() = waypoints[waypointsPassed + 1]
 
     private var lookaheadPoint: Point2D = currentWaypoint.point
 
     private val error: Double
-        get() = position.vectorTo(lookaheadPoint).angle
+        get() = (heading % 360) - toDegrees(position.vectorTo(lookaheadPoint).angle)
 
     fun getCurvature(deltaVector: Vector2D): Double {
         // update robot pose
@@ -34,7 +38,7 @@ class PurePursuitController(private val lookahead: Double,
 
         // check if current waypoint reached
         if (position.distance(currentWaypoint.point) <= currentWaypoint.radialTolerance) {
-            if (waypointN + 1 < waypoints.size) waypointN++
+            if (waypointsPassed + 1 < waypoints.size) waypointsPassed++
             else finished = true
         }
 
@@ -44,7 +48,7 @@ class PurePursuitController(private val lookahead: Double,
 
         lookaheadPoint = when (lookaheadPoints.size) {
             1 -> lookaheadPoints[0]
-            2 -> if(lookaheadPoints[0].distance(currentWaypoint.point) < lookaheadPoints[1].distance(currentWaypoint.point))
+            2 -> if (lookaheadPoints[0].distance(currentWaypoint.point) < lookaheadPoints[1].distance(currentWaypoint.point))
                 lookaheadPoints[0] else lookaheadPoints[1]
             else -> lookaheadPoint // this should only happen if there is no intersecting points. there should never be more than 2 points.
         }
