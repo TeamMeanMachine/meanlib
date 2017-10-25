40eedc94a332e2b07354834e503a04b5c2593912
tylerscheuble <tylerscheuble@gmail.com>
2017-10-25 16:43:19 -0700

    Clean up ClockServer a bit

--- src/main/kotlin/org/team2471/frc/lib/comm/ClockServer.kt
+++ src/main/kotlin/org/team2471/frc/lib/comm/ClockServer.kt
@@ -18,15 +18,14 @@ object ClockServer {
         Thread({
             val socket = DatagramSocket(port)
 
-            val recvPacket = DatagramPacket(kotlin.ByteArray(1), 1)
+            val recvPacket = DatagramPacket(ByteArray(1), 1)
 
-            val buffer = ByteArray(8)
-            val sendPacket = DatagramPacket(buffer, 8)
+            val sendPacket = DatagramPacket(ByteArray(8), 8)
 
             while(!Thread.currentThread().isInterrupted) {
                 socket.receive(recvPacket)
                 if(recvPacket.data[0] == 42.toByte()) {
-                    ByteBuffer.wrap(buffer).putDouble(timer())
+                    ByteBuffer.wrap(sendPacket.data).putDouble(timer())
                     sendPacket.address = recvPacket.address
                     sendPacket.port = recvPacket.port
                     socket.send(sendPacket)
