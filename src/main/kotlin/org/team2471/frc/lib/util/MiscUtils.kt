8c1011c92ec5f755c684090ad1b73784eeef064a
tylerscheuble <tylerscheuble@gmail.com>
2017-11-18 15:48:12 -0800

    Add measureTimeFPGA

---
+++ src/main/kotlin/org/team2471/frc/lib/util/MiscUtils.kt
@@ -0,0 +1,12 @@
+package org.team2471.frc.lib.util
+
+import edu.wpi.first.wpilibj.Timer
+
+/**
+ * Executes the given block and returns elapsed time in seconds.
+ */
+fun measureTimeFPGA(body: () -> Unit): Double {
+    val start = Timer.getFPGATimestamp()
+    body()
+    return Timer.getFPGATimestamp() - start
+}
