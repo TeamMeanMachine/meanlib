0a4086741237e457dc75bb689f566087b1a672f3
tylerscheuble <tyler@scheuble.us>
2018-09-11 15:55:05 -0700

    Polymorphic message handling

--- src/main/kotlin/org/team2471/frc/lib/control/experimental/next/SubsystemManager.kt
+++ src/main/kotlin/org/team2471/frc/lib/control/experimental/next/SubsystemManager.kt
@@ -17,15 +17,7 @@ object SubsystemManager {
         // spawn scheduler coroutine
         launch(MeanlibContext) {
             // handle incoming messages as they arrive
-            for (message in channel) {
-                when (message) {
-                    is Message.Enable -> handleEnableMessage()
-                    is Message.Disable -> handleDisableMessage()
-                    is Message.NewAction -> handleNewActionMessage(message)
-                    is Message.RegisterSubsystem -> handleRegisterSubsystemMessage(message)
-                    is Message.Clean -> handleCleanMessage(message)
-                }
-            }
+            for (message in channel) message.handle()
         }
     }
 
@@ -69,132 +61,129 @@ object SubsystemManager {
      * MESSAGE HANDLING
      */
 
-    private sealed class Message {
-        object Enable : Message()
+    private interface Message {
+        fun handle()
 
-        object Disable : Message()
+        object Enable : Message {
+            override fun handle() {
+                // do nothing if already enabled
+                if (isEnabled) return
+
+                // put all subsystems into default states
+                registry.forEach { it.launchDefaultAction() }
+
+                // update state
+                isEnabled = true
+            }
+        }
+
+        object Disable : Message {
+            override fun handle() {
+                // do nothing if already disabled
+                if (!isEnabled) return
+
+                // cancel all running actions
+                val e = CancellationException("Subsystem Coordinator Disabled")
+                cache.forEach { (_, job) -> job.cancel(e) }
+
+                // update state
+                isEnabled = false
+            }
+        }
 
         data class NewAction(
-                val locks: Set<SubsystemLock>,
-                val coroutineContext: CoroutineContext,
-                val body: suspend () -> Unit,
-                val cancelConflicts: Boolean,
-                val continuation: CancellableContinuation<Unit>
-        ) : Message()
+                private val locks: Set<SubsystemLock>,
+                private val callerContext: CoroutineContext,
+                private val body: suspend () -> Unit,
+                private val cancelConflicts: Boolean,
+                private val continuation: CancellableContinuation<Unit>
+        ) : Message {
+            override fun handle() {
+                // do nothing if currently disabled
+                if (!isEnabled) return
 
-        class RegisterSubsystem(val lock: SubsystemLock) : Message()
+                // subsystems required by use calls being used in calling coroutine
+                val previousHandles: Set<SubsystemLock> = callerContext[CoroutineRequirements] ?: emptySet()
+                // newly required subsystems (not previously required)
+                val newHandles = locks - previousHandles
+                // all subsystems
+                val allHandles = locks + previousHandles
 
-        data class Clean(val locks: Iterable<SubsystemLock>, val job: Job) : Message()
-    }
+                // find conflicts
+                val conflictingHandles = newHandles.filter { it in cache }
 
-    private fun handleEnableMessage() {
-        // do nothing if already enabled
-        if (isEnabled) return
-
-        // put all subsystems into default states
-        registry.forEach { it.launchDefaultAction() }
-
-        // update state
-        isEnabled = true
-    }
-
-    private fun handleDisableMessage() {
-        // do nothing if already disabled
-        if (!isEnabled) return
-
-        // cancel all running actions
-        val e = CancellationException("Subsystem Coordinator Disabled")
-        cache.forEach { (_, job) -> job.cancel(e) }
-
-        // update state
-        isEnabled = false
-    }
-
-    private fun handleNewActionMessage(message: Message.NewAction) {
-        // do nothing if currently disabled
-        if (!isEnabled) return
-
-        // destructure message object
-        val (locks, callerContext, body, cancelConflicts, continuation) = message
-
-        // subsystems required by use calls being used in calling coroutine
-        val previousHandles: Set<SubsystemLock> = callerContext[CoroutineRequirements] ?: emptySet()
-        // newly required subsystems (not previously required)
-        val newHandles = locks - previousHandles
-        // all subsystems
-        val allHandles = locks + previousHandles
-
-        // find conflicts
-        val conflictingHandles = newHandles.filter { it in cache }
-
-        if (!cancelConflicts && conflictingHandles.isNotEmpty()) {
-            continuation.resumeWithException(CancellationException("Action not allowed to cancel conflicts" +
-                    "{ ${conflictingHandles.joinToString { it.name }} }"))
-            return
-        }
-
-        val conflictingJobs = conflictingHandles.map { cache[it]!! }
-
-        // spawn action coroutine
-        val actionJob = launch(callerContext + CoroutineRequirements(allHandles), CoroutineStart.ATOMIC) {
-            try {
-                // cancel conflicts - action coroutines must not complete until
-                // it's conflict's jobs have finished execution
-                withContext(NonCancellable) {
-                    val e = CancellationException("Taking over subsystems " +
-                            "{ ${conflictingHandles.joinToString { it.name }} }")
-                    conflictingJobs.forEach { it.cancel(e) }
-                    conflictingJobs.forEach { it.join() }
+                if (!cancelConflicts && conflictingHandles.isNotEmpty()) {
+                    continuation.resumeWithException(CancellationException("Action not allowed to cancel conflicts" +
+                            "{ ${conflictingHandles.joinToString { it.name }} }"))
+                    return
                 }
 
-                // run provided code
-                body()
+                val conflictingJobs = conflictingHandles.map { cache[it]!! }
 
-                // resume calling coroutine
-                continuation.resume(Unit)
-            } catch (exception: Throwable) {
-                // pass exception to calling coroutine
-                continuation.resumeWithException(exception)
-            } finally {
-                // tell the scheduler that the action job has finished executing
-                channel.offer(Message.Clean(allHandles, coroutineContext[Job]!!))
+                // spawn action coroutine
+                val actionJob = launch(callerContext + CoroutineRequirements(allHandles), CoroutineStart.ATOMIC) {
+                    try {
+                        // cancel conflicts - action coroutines must not complete until
+                        // it's conflict's jobs have finished execution
+                        withContext(NonCancellable) {
+                            val e = CancellationException("Taking over subsystems " +
+                                    "{ ${conflictingHandles.joinToString { it.name }} }")
+                            conflictingJobs.forEach { it.cancel(e) }
+                            conflictingJobs.forEach { it.join() }
+                        }
+
+                        // run provided code
+                        body()
+
+                        // resume calling coroutine
+                        continuation.resume(Unit)
+                    } catch (exception: Throwable) {
+                        // pass exception to calling coroutine
+                        continuation.resumeWithException(exception)
+                    } finally {
+                        // tell the scheduler that the action job has finished executing
+                        channel.offer(Message.Clean(allHandles, coroutineContext[Job]!!))
+                    }
+                }
+
+                // write over state - future actions that use these locks will
+                // cancel and wait for the action job to complete
+                allHandles.forEach { cache[it] = actionJob }
+
+                // launch watchdog coroutine
+                launch(MeanlibContext) {
+                    var i = 0
+                    while (!actionJob.isCompleted) {
+                        if (actionJob.isCancelled) {
+                            if (i > 0) reportError("Action job in thread ${Thread.currentThread().name}" +
+                                    "hanging up subsystems { ${newHandles.joinToString { it.name }} } " +
+                                    "(${i * 2.5}s)", false)
+                            i++
+                        }
+
+                        delay(2500)
+                    }
+                }
             }
         }
 
-        // write over state - future actions that use these locks will
-        // cancel and wait for the action job to complete
-        allHandles.forEach { cache[it] = actionJob }
+        class RegisterSubsystem(private val lock: SubsystemLock) : Message {
+            override fun handle() {
+                registry.add(lock)
 
-        // launch watchdog coroutine
-        launch(MeanlibContext) {
-            var i = 0
-            while (!actionJob.isCompleted) {
-                if (actionJob.isCancelled) {
-                    if (i > 0) reportError("Action job in thread ${Thread.currentThread().name}" +
-                            "hanging up subsystems { ${newHandles.joinToString { it.name }} } " +
-                            "(${i * 2.5}s)", false)
-                    i++
-                }
-
-                delay(2500)
+                if (isEnabled && !cache.containsKey(lock)) lock.launchDefaultAction()
             }
         }
-    }
 
-    private fun handleRegisterSubsystemMessage(message: Message.RegisterSubsystem) {
-        val lock = message.lock
-        registry.add(lock)
-
-        if (isEnabled && !cache.containsKey(lock)) lock.launchDefaultAction()
-    }
-
-    private fun handleCleanMessage(message: Message.Clean) {
-        val (locks, job) = message
-        locks.filter { cache[it] === job }
-                .forEach {
-                    cache.remove(it)
-                    if (isEnabled) it.launchDefaultAction()
-                }
+        data class Clean(val locks: Iterable<SubsystemLock>, val job: Job) : Message {
+            override fun handle() {
+                locks.filter { cache[it] === job }
+                        .forEach {
+                            cache.remove(it)
+                            if (isEnabled) it.launchDefaultAction()
+                        }
+            }
+        }
     }
 
     private class CoroutineRequirements(
