bbd351d469343bbcd98d36d7c9e96bd2b2f5395d
tylerscheuble <tyler@scheuble.us>
2018-10-26 16:01:31 -0700

    Notifier based periodic loops

--- src/main/kotlin/org/team2471/frc/lib/control/experimental/CoroutineUtils.kt
+++ src/main/kotlin/org/team2471/frc/lib/control/experimental/CoroutineUtils.kt
@@ -1,24 +1,48 @@
 package org.team2471.frc.lib.control.experimental
 
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.Notifier
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.suspendCancellableCoroutine
+import org.team2471.frc.lib.util.measureTimeFPGA
 import java.util.concurrent.TimeUnit
 import kotlin.coroutines.experimental.coroutineContext
-import kotlin.math.roundToLong
 
 /**
- * Runs the provided [body] of code periodically per [period] ms.
+ * Runs the provided [body] of code periodically per [period] seconds.
  *
- * The [period] parameter defaults to 20ms.
- *
- * Additionally, a [condition] can be provided to allow termination of the loop without cancellation
- * of the command coroutine.
+ * The [period] parameter defaults to 0.02 seconds, or 20 milliseconds.
  */
-suspend inline fun periodic(period: Long = 20,
-                            condition: () -> Boolean = { true }, body: () -> Unit) {
-    while (condition()) {
-        body()
-        delay(period)
+suspend fun periodic(period: Double = 0.02, watchdog: Boolean = true, body: (Double) -> Boolean) {
+    val stackTrace = if (watchdog) Thread.currentThread().stackTrace else null
+
+    suspendCancellableCoroutine<Unit> { cont ->
+        var isFinished = false
+        val notifier = Notifier {
+            if (watchdog) {
+                val dt = measureTimeFPGA {
+                    isFinished = body(period)
+                }
+
+                if (dt > period) {
+                    DriverStation.reportWarning("Periodic loop took ${dt - period}s longer " +
+                            "than allowed period (${period}s).", stackTrace!!)
+                }
+            } else {
+                isFinished = body(period)
+            }
+
+            if (isFinished) {
+                cont.completeResume(Unit)
+            }
+        }
+
+        try {
+            notifier.startPeriodic(period)
+        } finally {
+            notifier.stop()
+        }
     }
 }
 
@@ -32,4 +56,4 @@ suspend fun parallel(vararg blocks: suspend () -> Unit) {
     }.forEach { it.join() }
 }
 
-suspend fun delaySeconds(time: Double) = delay((time * 1000).roundToLong())
+suspend fun delay(time: Double) = delay((time * 1000).toLong())
