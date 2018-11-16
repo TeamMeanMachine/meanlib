ec66a23cc91d08111bf20398be2b7ad1340900d4
tylerscheuble <tyler@scheuble.us>
2018-11-16 15:54:46 -0800

    Add "loop" alternative to periodic

--- src/main/kotlin/org/team2471/frc/lib/coroutines/CoroutineUtils.kt
+++ src/main/kotlin/org/team2471/frc/lib/coroutines/CoroutineUtils.kt
@@ -2,12 +2,17 @@ package org.team2471.frc.lib.coroutines
 
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.Notifier
-import kotlinx.coroutines.*
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 import org.team2471.frc.lib.util.measureTimeFPGA
 import kotlin.coroutines.resume
 
 /**
  * Runs the provided [body] of code periodically per [period] seconds.
+ *
+ * When [body] evaluates to true, the periodic loop is terminated.
  *
  * The [period] parameter defaults to 0.02 seconds, or 20 milliseconds.
  */
@@ -35,6 +40,35 @@ suspend fun periodic(period: Double = 0.02, watchdog: Boolean = true, body: (Dou
 
             if (isFinished) {
                 cont.resume(Unit)
+            }
+        }
+
+        notifier.startPeriodic(period)
+    }
+
+    notifier.stop()
+}
+
+/**
+ * Runs the provided [body] of code periodically per [period] seconds.
+ *
+ * The [period] parameter defaults to 0.02 seconds, or 20 milliseconds.
+ */
+suspend fun loop(period: Double = 0.02, watchdog: Boolean = true, body: (Double) -> Unit) {
+    val stackTrace = if (watchdog) Thread.currentThread().stackTrace else null
+
+    lateinit var notifier: Notifier
+    suspendCancellableCoroutine<Unit> { cont ->
+        notifier = Notifier {
+            if (watchdog) {
+                val dt = measureTimeFPGA { body(period) }
+
+                if (dt > period) DriverStation.reportWarning(
+                        "Periodic loop took ${dt - period}s longer " +
+                                "than allowed period (${period}s).", stackTrace!!
+                )
+            } else {
+                body(period)
             }
         }
 
