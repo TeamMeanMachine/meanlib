310b857ab1615fbc3688756fa221756d0c92b832
tylerscheuble <tyler@scheuble.us>
2018-11-16 21:03:23 -0800

    Removed some old stuff we don't use anymore

--- src/main/kotlin/org/team2471/frc/lib/math/Range.kt
+++ src/main/kotlin/org/team2471/frc/lib/math/Range.kt
@@ -1,9 +1,3 @@
 package org.team2471.frc.lib.math
 
 typealias DoubleRange = ClosedFloatingPointRange<Double>
-
-fun DoubleRange.clamp(value: Double) = when {
-    value < start -> start
-    value > endInclusive -> endInclusive
-    else -> value
-}
