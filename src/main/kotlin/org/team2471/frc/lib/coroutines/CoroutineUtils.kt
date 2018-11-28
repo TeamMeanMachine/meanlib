8f1511c417d11a1f8957fd9e6897c20e41a1f484
tylerscheuble <tyler@scheuble.us>
2018-11-28 01:04:03 -0800

    Remove notifier from periodic, introduce PeriodicScope

--- src/main/kotlin/org/team2471/frc/lib/coroutines/CoroutineUtils.kt
+++ src/main/kotlin/org/team2471/frc/lib/coroutines/CoroutineUtils.kt
@@ -1,52 +1,40 @@
 package org.team2471.frc.lib.coroutines
 
 import edu.wpi.first.wpilibj.DriverStation
-import edu.wpi.first.wpilibj.Notifier
 import kotlinx.coroutines.*
 import org.team2471.frc.lib.util.measureTimeFPGA
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
+
+private class BreakPeriodic : Throwable()
+
+class PeriodicScope @PublishedApi internal constructor(val period: Double) {
+    fun exitPeriodic(): Nothing = throw BreakPeriodic()
+}
 
 /**
  * Runs the provided [body] of code periodically per [period] seconds.
  *
- * The periodic loop will continue until a [PeriodicBreak] is thrown from the [body].
+ * The periodic loop will continue until [PeriodicScope.exitPeriodic] is called.
  *
  * The [period] parameter defaults to 0.02 seconds, or 20 milliseconds.
  *
  * If the [body] takes longer than the [period] to complete, a warning is printed. This can
- * be disabled by setting the [watchdog] parameter to false.
+ * be disabled by setting the [watchOverrun] parameter to false.
  */
-suspend fun periodic(period: Double = 0.02, watchdog: Boolean = true, body: () -> Unit) {
-    val stackTrace = if (watchdog) Thread.currentThread().stackTrace else null
+suspend inline fun periodic(period: Double = 0.02, watchOverrun: Boolean = true, body: PeriodicScope.() -> Unit) = try {
+    val scope = PeriodicScope(period)
 
-    lateinit var notifier: Notifier
-    suspendCancellableCoroutine<Unit> { cont ->
-        notifier = Notifier {
-            try {
-                if (watchdog) {
-                    val dt = measureTimeFPGA { body() }
-                    if (dt > period) DriverStation.reportWarning(
-                        "Periodic loop took ${dt - period}s longer " +
-                                "than allowed period (${period}s).", stackTrace!!
-                    )
-                } else {
-                    body()
-                }
-            } catch (_: PeriodicBreak) {
-                cont.resume(Unit)
-                notifier.stop()
-            } catch (e: Throwable) {
-                cont.resumeWithException(e)
-                notifier.stop()
-            }
-        }
+    while (true) {
+        val dt = measureTimeFPGA { body(scope) }
+        if (watchOverrun && dt > period) DriverStation.reportWarning(
+            "Code in periodic block ran ${period - dt} over it's period!",
+            Thread.currentThread().stackTrace
+        )
 
-        notifier.startPeriodic(period)
+        delay(period - dt)
     }
+} catch (_: BreakPeriodic) {
+    // do nothing
 }
-
-class PeriodicBreak : Throwable()
 
 /**
  * Suspends until [condition] evaluates to true.
@@ -62,7 +50,7 @@ suspend inline fun suspendUntil(pollingRate: Int = 20, condition: () -> Boolean)
  *
  * If one child is cancelled, the remaining children are stopped, and the exception is propagated upwards.
  */
-suspend fun parallel(vararg blocks: suspend () -> Unit) = coroutineScope {
+suspend inline fun parallel(vararg blocks: suspend () -> Unit) = coroutineScope {
     blocks.map { block ->
         launch { block() }
     }.forEach {
