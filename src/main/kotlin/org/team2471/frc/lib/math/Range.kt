566c4dcff45e7d9ace4321aec998732284d7beab
justinepend <justinetulip@gmail.com>
2019-02-16 15:00:37 -0800

    Add DoubleRange.intersects

--- src/main/kotlin/org/team2471/frc/lib/math/Range.kt
+++ src/main/kotlin/org/team2471/frc/lib/math/Range.kt
@@ -1,3 +1,6 @@
 package org.team2471.frc.lib.math
 
 typealias DoubleRange = ClosedFloatingPointRange<Double>
+
+fun DoubleRange.intersects(other: DoubleRange): Boolean =
+        start in other || endInclusive in other
