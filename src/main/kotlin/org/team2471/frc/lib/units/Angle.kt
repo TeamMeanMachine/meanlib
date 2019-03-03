d22a56e83481eb5d1110fc93b33a8821d1ee9cbd
tyler <tyler@scheuble.us>
2019-03-02 16:41:15 -0800

    Use degrees as the base in Angle

--- src/main/kotlin/org/team2471/frc/lib/units/Angle.kt
+++ src/main/kotlin/org/team2471/frc/lib/units/Angle.kt
@@ -1,21 +1,21 @@
 package org.team2471.frc.lib.units
 
-inline class Angle(val asRadians: Double) {
-    operator fun plus(other: Angle) = Angle(asRadians + other.asRadians)
+inline class Angle(val asDegrees: Double) {
+    operator fun plus(other: Angle) = Angle(asDegrees + other.asDegrees)
 
-    operator fun minus(other: Angle) = Angle(asRadians - other.asRadians)
+    operator fun minus(other: Angle) = Angle(asDegrees - other.asDegrees)
 
-    operator fun times(factor: Double) = Angle(asRadians * factor)
+    operator fun times(factor: Double) = Angle(asDegrees * factor)
 
-    operator fun div(factor: Double) = Angle(asRadians / factor)
+    operator fun div(factor: Double) = Angle(asDegrees / factor)
 
-    operator fun rem(other: Angle) = Angle(asRadians % other.asRadians)
+    operator fun rem(other: Angle) = Angle(asDegrees % other.asDegrees)
 
     operator fun unaryPlus() = this
 
-    operator fun unaryMinus() = Angle(-asRadians)
+    operator fun unaryMinus() = Angle(-asDegrees)
 
-    operator fun compareTo(other: Angle) = asRadians.compareTo(other.asRadians)
+    operator fun compareTo(other: Angle) = asDegrees.compareTo(other.asDegrees)
 
     override fun toString() = "$asDegrees degrees"
 
@@ -25,7 +25,7 @@ inline class Angle(val asRadians: Double) {
 
     fun tan() = Angle.tan(this)
 
-    fun wrap() = Angle(Math.IEEEremainder(asRadians, 2 * Math.PI))
+    fun wrap() = Angle(Math.IEEEremainder(asDegrees, 360.0))
 
     companion object {
         @JvmStatic
@@ -55,8 +55,8 @@ inline class Angle(val asRadians: Double) {
 }
 
 // constructors
-inline val Number.radians get() = Angle(this.toDouble())
-inline val Number.degrees get() = Angle(Math.toRadians(this.toDouble()))
+inline val Number.radians get() = Angle(Math.toRadians(this.toDouble()))
+inline val Number.degrees get() = Angle(this.toDouble())
 
 // destructors
-inline val Angle.asDegrees get() = Math.toDegrees(asRadians)
+inline val Angle.asRadians get() = Math.toRadians(asDegrees)
