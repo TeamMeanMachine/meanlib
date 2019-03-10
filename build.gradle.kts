04053800e37252106320552ac574f57e8241f43a
tylerscheuble <tyler@scheuble.us>
2019-03-09 19:43:32 -0800

    Update FRC libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ buildscript {
     repositories {
         mavenCentral()
     }
+
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.0")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
@@ -36,10 +37,10 @@ dependencies {
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0")
 
     // frc libs
-    implementation("edu.wpi.first.wpilibj:wpilibj-java:2019.3.1")
-    implementation("edu.wpi.first.hal:hal-java:2019.3.1")
-    implementation("edu.wpi.first.wpiutil:wpiutil-java:2019.3.1")
-    implementation("edu.wpi.first.ntcore:ntcore-java:2019.3.1")
+    implementation("edu.wpi.first.wpilibj:wpilibj-java:2019.4.1")
+    implementation("edu.wpi.first.hal:hal-java:2019.4.1")
+    implementation("edu.wpi.first.wpiutil:wpiutil-java:2019.4.1")
+    implementation("edu.wpi.first.ntcore:ntcore-java:2019.4.1")
     implementation("com.ctre.phoenix:api-java:5.12.0")
 
     // other
