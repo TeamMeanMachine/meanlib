c0d3381c2411dc587eafdd95af4ef4076846b46b
TMMShared <camasinfiniteresource@gmail.com>
2024-11-24 14:27:23 -0800

    removed config stuff in loggedCANcoder.

--- src/main/kotlin/org/team2471/frc/lib/sensors/canCoder/LoggedCANCoderReal.kt
+++ src/main/kotlin/org/team2471/frc/lib/sensors/canCoder/LoggedCANCoderReal.kt
@@ -1,42 +1,24 @@
 package org.team2471.frc.lib.sensors.canCoder
 
-import com.ctre.phoenix6.configs.CANcoderConfiguration
 import com.ctre.phoenix6.hardware.CANcoder
-import com.ctre.phoenix6.signals.SensorDirectionValue
 import org.team2471.frc.lib.units.Angle
 import org.team2471.frc.lib.units.degrees
 
 class LoggedCANCoderReal(id: Int, canbus: String? = ""): LoggedCANCoderIO {
     private val canCoder = CANcoder(id, canbus)
-    private val config  = CANcoderConfiguration()
-    private var appliedConfig = CANcoderConfiguration()
-    private val configUnsaved: Boolean get() = config.serialize() != appliedConfig.serialize()
     override var simAngleSupplier: () -> Angle = {0.0.degrees}
+    private var inverted = false
 
     override fun updateInputs(inputs: LoggedCANCoderIO.CANCoderIOInputs) {
-        inputs.position = canCoder.position.value
-        inputs.velocity = canCoder.velocity.value
-        applyConfigIfChanged()
-    }
-
-    override fun setMagnetSensorOffset(offset: Double) {
-        config.MagnetSensor.MagnetOffset = offset
+        inputs.position = canCoder.position.value * if (inverted) -1.0 else 1.0
+        inputs.velocity = canCoder.velocity.value * if (inverted) -1.0 else 1.0
     }
 
     override fun setInverted(invert: Boolean) {
-        config.MagnetSensor.SensorDirection =
-            if (invert) SensorDirectionValue.CounterClockwise_Positive
-            else SensorDirectionValue.Clockwise_Positive
+        inverted = invert
     }
 
     override fun setPosition(position: Double) {
         canCoder.setPosition(position)
-    }
-
-    private fun applyConfigIfChanged() = if (configUnsaved) applyConfig() else {}
-
-    private fun applyConfig() {
-        canCoder.configurator.apply(config)
-        appliedConfig = config
     }
 }
