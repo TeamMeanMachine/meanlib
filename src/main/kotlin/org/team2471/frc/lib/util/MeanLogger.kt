26b614a4ee7735fce0b3b81002091717643ef7c2
TMMShared <camasinfiniteresource@gmail.com>
2025-01-27 14:58:07 -0800

    MeanLogger no error

--- src/main/kotlin/org/team2471/frc/lib/util/MeanLogger.kt
+++ src/main/kotlin/org/team2471/frc/lib/util/MeanLogger.kt
@@ -25,241 +25,241 @@ object MeanLogger {
 
     fun recordOutput(key: String?, value: ByteArray?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<ByteArray?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Boolean) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: BooleanSupplier) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: BooleanArray?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<BooleanArray?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Int) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: IntSupplier) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: IntArray?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<IntArray?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Long) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: LongSupplier) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: LongArray?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<LongArray?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Float) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: FloatArray?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<FloatArray?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Double) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: DoubleSupplier) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: DoubleArray?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<DoubleArray?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: String?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<String?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Array<Array<String?>?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <E : Enum<E>?> recordOutput(key: String?, value: E) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <E : Enum<E>?> recordOutput(key: String?, value: Array<E>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <E : Enum<E>?> recordOutput(key: String?, value: Array<Array<E>?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <U : edu.wpi.first.units.Unit?> recordOutput(key: String?, value: Measure<U>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <T> recordOutput(key: String?, struct: Struct<T>?, value: T) {
         synchronized(lock) {
-            Logger.recordOutput(key, struct, value)
+            try {Logger.recordOutput(key, struct, value)} catch (_:Exception) {}
         }
     }
 
     fun <T> recordOutput(key: String?, struct: Struct<T>?, vararg value: T) {
         synchronized(lock) {
-            Logger.recordOutput(key, struct, *value)
+            try {Logger.recordOutput(key, struct, *value)} catch (_:Exception) {}
         }
     }
 
     fun <T> recordOutput(key: String?, struct: Struct<T>?, value: Array<Array<T>?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, struct, value)
+            try {Logger.recordOutput(key, struct, value)} catch (_:Exception) {}
         }
     }
 
     fun <T : WPISerializable?> recordOutput(key: String?, value: T) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <T : StructSerializable?> recordOutput(key: String?, vararg value: T) {
         synchronized(lock) {
-            Logger.recordOutput(key, *value)
+            try {Logger.recordOutput(key, *value)} catch (_:Exception) {}
         }
     }
 
     fun <T : StructSerializable?> recordOutput(key: String?, value: Array<Array<T>?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <R : Record?> recordOutput(key: String?, value: R) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun <R : Record?> recordOutput(key: String?, vararg value: R) {
         synchronized(lock) {
-            Logger.recordOutput(key, *value)
+            try {Logger.recordOutput(key, *value)} catch (_:Exception) {}
         }
     }
 
     fun <R : Record?> recordOutput(key: String?, value: Array<Array<R>?>?) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: LoggedMechanism2d) {
         synchronized(lock) {
-            Logger.recordOutput(key, value)
+            try {Logger.recordOutput(key, value)} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Vector2L) {
         synchronized(lock) {
-            Logger.recordOutput(key, value.asMeters.toTranslation2d())
+            try {Logger.recordOutput(key, value.asMeters.toTranslation2d())} catch (_:Exception) {}
         }
     }
 
     fun recordOutput(key: String?, value: Vector2L, angle: Angle) {
         synchronized(lock) {
-            Logger.recordOutput(key, value.asMeters.toPose2d(angle))
+            try {Logger.recordOutput(key, value.asMeters.toPose2d(angle))} catch (_:Exception) {}
         }
     }
 }
