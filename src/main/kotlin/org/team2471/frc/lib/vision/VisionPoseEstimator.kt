d8d0ef650f1ea924460e4e04fcedb57b5d82fa31
Chair <teammoorefamily@gmail.com>
2025-02-20 20:07:59 -0800

    quest should work better

--- src/main/kotlin/org/team2471/frc/lib/vision/VisionPoseEstimator.kt
+++ src/main/kotlin/org/team2471/frc/lib/vision/VisionPoseEstimator.kt
@@ -7,75 +7,94 @@ import edu.wpi.first.math.Nat
 import edu.wpi.first.math.StateSpaceUtil
 import edu.wpi.first.math.VecBuilder
 import edu.wpi.first.math.estimator.ExtendedKalmanFilter
-import edu.wpi.first.math.geometry.Pose2d
-import edu.wpi.first.math.geometry.Twist2d
 import edu.wpi.first.math.numbers.N1
 import edu.wpi.first.math.numbers.N2
-import edu.wpi.first.networktables.NetworkTableInstance
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.team2471.frc.lib.coroutines.periodic
 import org.team2471.frc.lib.math.*
-import org.team2471.frc.lib.units.Angle
 import org.team2471.frc.lib.units.asMeters
 import org.team2471.frc.lib.units.meters
 import org.team2471.frc.lib.util.MeanLogger
-import org.team2471.frc.lib.util.MovingAverageTwist2d
 import org.team2471.frc.lib.util.getRealFPGATimestamp
 
 // Huge thanks to 1678, thank you so much for pioneering this
 // I would say you saved me a huge headache, but I've already been through that
 
 class VisionPoseEstimator(
-    val defaultStateStdDevs: Matrix<N2, N1> = VecBuilder.fill(0.2, 0.2),
-    val defaultMeasurementStdDevs: Matrix<N2, N1> = VecBuilder.fill(0.2, 0.2)
+    odomStdDev: Matrix<N2, N1> = VecBuilder.fill(0.2, 0.2),
+    defaultVisionStdDev: Matrix<N2, N1> = VecBuilder.fill(0.2, 0.2),
+    questStdDev: Matrix<N2, N1> = VecBuilder.fill(0.005, 0.005)
 ) {
 
     val latestPos: Vector2L
         get() = try {
-            (offsetHistory[offsetHistory.lastKey()]?.let { odomPosHistory[odomPosHistory.lastKey()]?.plus(it) })
+            (odomOffsetHistory[odomOffsetHistory.lastKey()]?.let { odomPosHistory[odomPosHistory.lastKey()]?.plus(it) })
                 ?: Vector2L.Zeros
         } catch (e: Exception) {
             Vector2L.Zeros
         }
 
+    private var isQuestConnected: Boolean = false
+    private var doesQuestExist: Boolean = false
+
     val odomPosHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L> = InterpolatingTreeMap(50)
-    val offsetHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L> = InterpolatingTreeMap(50)
+    val odomOffsetHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L> = InterpolatingTreeMap(50)
+    val questPosHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L> = InterpolatingTreeMap(50)
+    val questOffsetHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L> = InterpolatingTreeMap(50)
 
-    var inReset = false
-    val isEnabled = false
+    private var inReset = false
 
-    val kalmanFilter = ExtendedKalmanFilter(
+    private val odomKalmanFilter = ExtendedKalmanFilter(
         Nat.N2(), // Dimensions of output (x, y)
         Nat.N2(), // Dimensions of predicted error shift (dx, dy) (always 0),
         Nat.N2(), // Dimensions of vision (x, y),
         { x: Matrix<N2?, N1?>?, u: Matrix<N2?, N1?>? -> u }, // The derivative of the output is predicted shift (always 0),
         { x: Matrix<N2?, N1?>?, u: Matrix<N2?, N1?>? -> x }, // The output is position (x, y),
-        defaultStateStdDevs, // Standard deviation of position (uncertainty propagation with no vision),
-        defaultMeasurementStdDevs,// Standard deviation of vision measurements,
+        odomStdDev, // Standard deviation of position (uncertainty propagation with no vision),
+        defaultVisionStdDev,// Standard deviation of vision measurements,
+        0.02
+    )
+
+    private val questKalmanFilter = ExtendedKalmanFilter(
+        Nat.N2(), // Dimensions of output (x, y)
+        Nat.N2(), // Dimensions of predicted error shift (dx, dy) (always 0),
+        Nat.N2(), // Dimensions of vision (x, y),
+        { x: Matrix<N2?, N1?>?, u: Matrix<N2?, N1?>? -> u }, // The derivative of the output is predicted shift (always 0),
+        { x: Matrix<N2?, N1?>?, u: Matrix<N2?, N1?>? -> x }, // The output is position (x, y),
+        questStdDev, // Standard deviation of position (uncertainty propagation with no vision),
+        defaultVisionStdDev,// Standard deviation of vision measurements,
         0.02
     )
 
     fun reset(
         newPos: Vector2L,
         currentTimestampSeconds: Double = getRealFPGATimestamp(),
-        odometryReset: Boolean = false
+        odometryReset: Boolean = false,
+        questReset: Boolean = false
     ) {
         inReset = true
-        val prevOdomPose = odomPosHistory.lastEntry()
 
-        offsetHistory.clear()
-        if (odometryReset) {
-            odomPosHistory.clear()
-            odomPosHistory[InterpolatingDouble(currentTimestampSeconds)] = newPos
-            offsetHistory[InterpolatingDouble(currentTimestampSeconds)] = Vector2L.Zeros
-        } else {
-            offsetHistory[InterpolatingDouble(currentTimestampSeconds)] = newPos - prevOdomPose.value
+        resetHistories(odomPosHistory, odomOffsetHistory, newPos, currentTimestampSeconds, odometryReset)
+        if (doesQuestExist) {
+            resetHistories(questPosHistory, questOffsetHistory, newPos, currentTimestampSeconds, questReset)
         }
+
+//        val prevQuestPose =
 
 //        predictedVelocity = Twist2d()
 
         inReset = false
+    }
+
+    private fun resetHistories(baseHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L>, offsetHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L>, newPos: Vector2L, currentTimestampSeconds: Double, baseReset: Boolean) {
+        val prevPos = baseHistory.lastEntry().value
+
+        offsetHistory.clear()
+        if (baseReset) {
+            baseHistory.clear()
+            baseHistory[InterpolatingDouble(currentTimestampSeconds)] = newPos
+            offsetHistory[InterpolatingDouble(currentTimestampSeconds)] = Vector2L.Zeros
+        } else {
+            offsetHistory[InterpolatingDouble(currentTimestampSeconds)] = newPos - prevPos
+        }
     }
 
     fun updateOdometry(
@@ -83,11 +102,23 @@ class VisionPoseEstimator(
         odometryPose: Vector2L,
     ) {
         if (!inReset) {
-            //        println(measuredVelocity)
             odomPosHistory[InterpolatingDouble(currentTimestampSeconds)] = odometryPose
-            kalmanFilter.predict(VecBuilder.fill(0.0, 0.0), 0.02)
-            //        this.predictedVelocity = predictedVelocity
-            MeanLogger.recordOutput("odomPose", latestPos)
+            odomKalmanFilter.predict(VecBuilder.fill(0.0, 0.0), 0.02)
+        }
+    }
+
+    fun updateQuest(
+        questPose: Vector2L,
+        isQuestConnected: Boolean,
+        currentTimestampSeconds: Double = getRealFPGATimestamp()
+    ) {
+        this.isQuestConnected = isQuestConnected
+        if (this.isQuestConnected) {
+            doesQuestExist = true
+            if (!inReset) {
+                questPosHistory[InterpolatingDouble(currentTimestampSeconds)] = questPose
+                questKalmanFilter.predict(VecBuilder.fill(0.0, 0.0), 0.02)
+            }
         }
     }
 
@@ -105,28 +136,34 @@ class VisionPoseEstimator(
 
     fun addVisionUpdate(globalPose: GlobalPose) {
         if (!inReset) {
-            if (globalPose == GlobalPose.EmptyGlobalPose) {
-                return
+            addVisionUpdateToHistory(globalPose, odomKalmanFilter, odomPosHistory, odomOffsetHistory)
+            if (doesQuestExist) {
+                addVisionUpdateToHistory(globalPose, questKalmanFilter, questPosHistory, questOffsetHistory)
             }
-            try {
-                val odometryPose = odomPosHistory.getInterpolated(InterpolatingDouble(globalPose.timestampSeconds))
-                val odometryOffset = globalPose.pose.translation.asVector2().meters - odometryPose
+        }
+    }
 
-                val stdDevs = VecBuilder.fill(globalPose.stdDev, globalPose.stdDev)
-                val covarianceMatrix = StateSpaceUtil.makeCovarianceMatrix(Nat.N2(), stdDevs)
+    private fun addVisionUpdateToHistory(globalPose: GlobalPose, kalmanFilter: ExtendedKalmanFilter<N2, N2, N2>, baseHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L>, offsetHistory: InterpolatingTreeMap<InterpolatingDouble, Vector2L>) {
+        if (globalPose == GlobalPose.EmptyGlobalPose) {
+            return
+        }
+        try {
+            val basePose = baseHistory.getInterpolated(InterpolatingDouble(globalPose.timestampSeconds))
+            val offset = globalPose.pose.translation.asVector2().meters - basePose
 
-                kalmanFilter.correct(
-                    VecBuilder.fill(0.0, 0.0),
-                    VecBuilder.fill(odometryOffset.x.asMeters, odometryOffset.y.asMeters),
-                    covarianceMatrix
-                )
+            val stdDevs = VecBuilder.fill(globalPose.stdDev, globalPose.stdDev)
+            val covarianceMatrix = StateSpaceUtil.makeCovarianceMatrix(Nat.N2(), stdDevs)
+
+            kalmanFilter.correct(
+                VecBuilder.fill(0.0, 0.0),
+                VecBuilder.fill(offset.x.asMeters, offset.y.asMeters),
+                covarianceMatrix
+            )
 
 
-                offsetHistory[InterpolatingDouble(globalPose.timestampSeconds)] =
-                    Vector2L(kalmanFilter.getXhat(0).meters, kalmanFilter.getXhat(1).meters)
-            } catch (e: Exception) {
-                println("Error updating vision pose: $e")
-            }
+            offsetHistory[InterpolatingDouble(globalPose.timestampSeconds)] = Vector2L(kalmanFilter.getXhat(0).meters, kalmanFilter.getXhat(1).meters)
+        } catch (e: Exception) {
+            println("Error updating vision pose: $e")
         }
     }
 }
--- src/main/kotlin/org/team2471/frc/lib/vision/VisionUtils.kt
+++ src/main/kotlin/org/team2471/frc/lib/vision/VisionUtils.kt
@@ -6,31 +6,14 @@ import edu.wpi.first.math.geometry.Rotation2d
 import org.photonvision.simulation.SimCameraProperties
 import org.team2471.frc.lib.interpolation.SplineInterpolator
 import org.team2471.frc.lib.math.DoubleRange
+import kotlin.math.ln
+import kotlin.math.pow
 
+//todo update
 val fieldXRangeM = DoubleRange(0.0, 16.54)
 val fieldYRangeM = DoubleRange(0.0, 8.21)
 
-/**
- * A curve representing the standard deviation of PhotonVision pose results as a function of average tag area (in percent).
- * @author Thatcher Moore
- */
-val photonStDevAreaCurve: SplineInterpolator = SplineInterpolator(
-    // dist in meters
-    mutableMapOf(
-        1.69212 to 0.0000648,
-        0.89687 to 0.0000722,
-        0.59865 to 0.0002967,
-        0.15958 to 4.8117529,
-        0.11406 to 0.0007479,
-        0.11092 to 0.0026701
-    )
-)
-
-val photonStdDevAreaCurve: SplineInterpolator = SplineInterpolator(
-    mutableMapOf(
-        0.0 to 0.0
-    )
-)
+val photonStdDevCalculator: StdDevCalculator = StdDevCalculator(1.0, Pair(0.1, 0.001), Pair(5.0, 0.5))
 
 fun AprilTagFieldLayout.removeTags(tagIDs: IntArray): AprilTagFieldLayout {
     return AprilTagFieldLayout(this.tags.filter { it.ID !in tagIDs }, this.fieldLength, this.fieldWidth)
@@ -42,4 +25,33 @@ fun AprilTagFieldLayout.removeTags(tagIDs: Collection<Int>): AprilTagFieldLayout
 
 fun Pose2d.isOnField(): Boolean {
     return fieldXRangeM.contains(this.x) && fieldYRangeM.contains(this.y)
+}
+
+
+/**
+ * Uses a curve and a multiplier to calculate standard deviation of a vision measurement.
+ *
+ * The formula used is m * f(d) / n
+ * - m is a specified multiplier
+ * - n is the number of tags seen
+ * - f(d) is standard deviation as a function of distance (meters) and is of the form ad^b
+ *
+ * @param multiplier an arbitrary multiplier to scale stdDev calculation. Must be greater than 0
+ * @param point1 An average distance in meters paired with a standard deviation. This point is meant to represent a close up, trusted result. Used to generate f(d). Both values must be less than the corresponding values in point2 and greater than 0.
+ * @param point2 An average distance in meters paired with a standard deviation. This point is meant to represent a far away, untrustworthy result. Used to generate f(d). Both values must be greater than the corresponding values in point1 and greater than 0.
+ *
+ * @throws IllegalArgumentException if any of the arguments are outside of the specified bounds
+ *
+ * @author Thatcher Moore
+ */
+class StdDevCalculator(var multiplier: Double, point1: Pair<Double, Double>, point2: Pair<Double, Double>) {
+
+    val b = ln(point1.second / point2.second) / ln(point1.first / point1.second)
+    val a = point1.second / point1.first.pow(b)
+
+    fun calculateStdDev(avgDistM: Double, numTagsSeen: Int) = multiplier * a * avgDistM.pow(b) / numTagsSeen
+
+    init {
+        require(multiplier > 0 && point1.first < point2.first && point1.second < point2.second && point1.first > 0.0 && point1.second > 0.0)
+    }
 }
