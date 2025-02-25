0b216d32e9fc5fdd01257f5c1ba734720438fda0
TMMShared <camasinfiniteresource@gmail.com>
2025-02-24 19:40:07 -0800

    drive with velocity test

--- src/main/kotlin/org/team2471/frc/lib/actuators/MotorControllerIO.kt
+++ src/main/kotlin/org/team2471/frc/lib/actuators/MotorControllerIO.kt
@@ -42,7 +42,7 @@ interface MotorControllerIO {
     fun config_kD(d: Double, simD: Double? = 0.0)
     fun config_kI(i: Double, simI: Double? = 0.0)
     fun config_kF(f: Double, simF: Double? = 0.0)
-    fun config_kV(v: Double)
+    fun config_kV(v: Double) {}
     fun configSim(motor: DCMotor, jKgMetersSquared: Double) {}
     fun currentLimit(continuousLimit: Int, peakLimit: Int, peakDuration: Double)
     fun encoderContinuous(continuous: Boolean) {}
