d49905ce7ea79fd3202bd7d37e1ec7dd8be79cf5
TMMShared <camasinfiniteresource@gmail.com>
2026-02-02 20:14:28 -0800

    Turret wrap attempt

---
+++ src/main/kotlin/org/team2471/frc/lib/coroutines/CoroutineUtil.kt
@@ -0,0 +1,77 @@
+package org.team2471.frc.lib.coroutines
+
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj.Watchdog
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
+
+class PeriodicScope @PublishedApi internal constructor(val period: Double) {
+    @PublishedApi
+    internal var isDone = false
+
+    fun stop() {
+        isDone = true
+    }
+}
+
+
+
+
+
+
+/**
+ * Runs the provided [body] of code periodically per [period] seconds.
+ *
+ * The provided [body] loop will continue to loop until [PeriodicScope.stop] is called, or an exception is thrown.
+ * Note that if [PeriodicScope.stop] is called the body will continue to run to the end of the loop. If your
+ * intention is to exit the code early, insert a return after calling [PeriodicScope.stop].
+ *
+ * The [period] parameter defaults to 0.02 seconds, or 20 milliseconds.
+ *
+ * If the [body] takes longer than the [period] to complete, a warning is printed. This can
+ * be disabled by setting the [watchOverrun] parameter to false.
+ */
+suspend inline fun periodic(
+    period: Double = 0.02,
+    watchOverrun: Boolean = false,
+    crossinline body: PeriodicScope.() -> Unit
+) {
+    val scope = PeriodicScope(period)
+
+    val watchdog = if (watchOverrun) {
+        Watchdog(period) { DriverStation.reportWarning("Periodic loop overrun", true) }
+    } else {
+        null
+    }
+
+    while (true) {
+        watchdog?.reset()
+        val dt = measureTimeFPGA {
+            body(scope)
+        }
+        if (scope.isDone) break
+        val remainder = period - dt
+        if (remainder <= 0.0) {
+            yield()
+        } else {
+            delay(remainder)
+        }
+    }
+}
+
+/**
+ * Executes the given block and returns elapsed time in seconds.
+ */
+inline fun measureTimeFPGA(body: () -> Unit): Double {
+    val start = Timer.getFPGATimestamp()
+    body()
+    return Timer.getFPGATimestamp() - start
+}
+
+/**
+ * Suspends the coroutine for [time] seconds.
+ *
+ * @see kotlinx.coroutines.delay
+ */
+suspend inline fun delay(time: Double) = delay((time * 1000).toLong())
