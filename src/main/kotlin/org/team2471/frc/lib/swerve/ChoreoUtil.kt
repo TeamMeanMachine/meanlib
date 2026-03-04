fc17ad9be9608fc3e4976c9118184be4d1f51ff5
TMMShared <camasinfiniteresource@gmail.com>
2026-03-03 21:17:12 -0800

    Some auto changes. Found bug with side to side flip that needs to be fixed

--- src/main/kotlin/org/team2471/frc/lib/swerve/ChoreoUtil.kt
+++ src/main/kotlin/org/team2471/frc/lib/swerve/ChoreoUtil.kt
@@ -5,7 +5,6 @@ import choreo.trajectory.Trajectory
 import choreo.util.ChoreoAllianceFlipUtil.Flipper
 import java.util.ArrayList
 import java.util.Arrays
-import java.util.function.DoubleUnaryOperator
 
 /**
  * Instead of Rotating or Mirroring across the center of the field,
@@ -25,7 +24,7 @@ fun Trajectory<SwerveSample>.sideToSideFlip(doFlip: Boolean): Trajectory<SwerveS
 
 fun SwerveSample.sideToSideFlip(): SwerveSample {
     val rotated = this.rotateAround()
-    val rotatedAndMirrored = rotated.mirror()
+    val rotatedAndMirrored = rotated.modifiedMirror()
     return rotatedAndMirrored
 }
 
@@ -43,12 +42,13 @@ fun SwerveSample.rotateAround(): SwerveSample {
         -this.ax,
         -this.ay,
         this.alpha,
-        Arrays.stream(this.moduleForcesX()).map(DoubleUnaryOperator { x: Double -> -x }).toArray(),
-        Arrays.stream(this.moduleForcesY()).map(DoubleUnaryOperator { y: Double -> -y }).toArray()
+        Arrays.stream(this.moduleForcesX()).map { x: Double -> -x }.toArray(),
+        Arrays.stream(this.moduleForcesY()).map { y: Double -> -y }.toArray()
     )
 }
 
-fun SwerveSample.mirror(): SwerveSample {
+/** Doesn't perfectly match choreo mirror. Module forces do not change arrangement */
+fun SwerveSample.modifiedMirror(): SwerveSample {
     return SwerveSample(
         this.t,
         Flipper.MIRRORED.flipX(this.x),
@@ -63,18 +63,18 @@ fun SwerveSample.mirror(): SwerveSample {
         // Mirrored
         // -FR, -FL, -BR, -BL
         doubleArrayOf(
-            -this.moduleForcesX()[1],
             -this.moduleForcesX()[0],
-            -this.moduleForcesX()[3],
-            -this.moduleForcesX()[2]
+            -this.moduleForcesX()[1],
+            -this.moduleForcesX()[2],
+            -this.moduleForcesX()[3]
         ),  // FL, FR, BL, BR
         // Mirrored
         // FR, FL, BR, BL
         doubleArrayOf(
-            this.moduleForcesY()[1],
             this.moduleForcesY()[0],
-            this.moduleForcesY()[3],
-            this.moduleForcesY()[2]
+            this.moduleForcesY()[1],
+            this.moduleForcesY()[2],
+            this.moduleForcesY()[3]
         )
     )
 }
