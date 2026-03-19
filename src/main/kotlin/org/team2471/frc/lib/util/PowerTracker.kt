d9a42c9c24393d342137324c0cc43c50fd71fe29
Chair <teammoorefamily@gmail.com>
2026-03-19 10:09:31 -0700

    fixed powertracker. need to test voltage w/ intake roller.

--- src/main/kotlin/org/team2471/frc/lib/util/PowerTracker.kt
+++ src/main/kotlin/org/team2471/frc/lib/util/PowerTracker.kt
@@ -10,12 +10,13 @@ class PowerTracker {
     var totalCharge = 0.0
     var totalPower = 0.0
 
-    fun addMotors(name: String, currentSupplier: () -> Double, numMotors: Int = 1) {
-        motors.add(MotorTracker(name, currentSupplier, numMotors))
+    // Don't call if sim
+    fun addMotors(name: String, currentSupplier: () -> Double, numMotors: Int = 1, voltageSupplier: () -> Double? = {null}) {
+        motors.add(MotorTracker(name, currentSupplier, voltageSupplier, numMotors))
     }
 
     fun update(currentVoltage: Double) {
-        Logger.recordOutput("PowerTracker/Battery Voltage", currentVoltage, "volts")
+        Logger.recordOutput("PowerTracker/Rio Voltage", currentVoltage, "volts")
         motors.forEach { it ->
             it.update(currentVoltage)
             totalCharge += it.totalCharge
@@ -26,13 +27,15 @@ class PowerTracker {
     }
 }
 
-private class MotorTracker(val name: String, val currentSupplier: () -> Double, val numMotors: Int = 1) {
+private class MotorTracker(val name: String, val currentSupplier: () -> Double, val voltageSupplier: () -> Double? = {null}, val numMotors: Int = 1) {
     var totalCharge = 0.0
     var totalPower = 0.0
 
     fun update(currentVoltage: Double) {
         val current = numMotors * currentSupplier.invoke()
-        val power = currentVoltage * numMotors * currentSupplier.invoke()
+        val power = (voltageSupplier.invoke() ?: currentVoltage) * numMotors * currentSupplier.invoke()
+
+        Logger.recordOutput("PowerTracker/${name} Supply Voltage", voltageSupplier.invoke() ?: 0.0, "volts")
 
         Logger.recordOutput("PowerTracker/${name} Current Draw", current, "amps")
         Logger.recordOutput("PowerTracker/${name} Power Draw", power, "watts")
