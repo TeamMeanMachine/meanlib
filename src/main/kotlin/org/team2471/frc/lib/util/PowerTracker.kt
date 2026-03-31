a6a25e2f4a381d5ba2dda783986cae0e3a6074e5
TMMShared <camasinfiniteresource@gmail.com>
2026-03-30 21:00:38 -0700

    murdered power tracker
    also added other intake motor

--- src/main/kotlin/org/team2471/frc/lib/util/PowerTracker.kt
+++ src/main/kotlin/org/team2471/frc/lib/util/PowerTracker.kt
@@ -15,37 +15,37 @@ class PowerTracker {
         motors.add(MotorTracker(name, currentSupplier, voltageSupplier, numMotors))
     }
 
-    fun update(currentVoltage: Double) {
-        Logger.recordOutput("PowerTracker/Rio Voltage", currentVoltage, "volts")
+    fun update(/*currentVoltage: Double,*/ delta: Double = 0.02) {
+//        Logger.recordOutput("PowerTracker/Rio Voltage", currentVoltage, "volts")
         totalCharge = 0.0
-        totalPower = 0.0
+//        totalPower = 0.0
         motors.forEach { it ->
-            it.update(currentVoltage)
+            it.update(/*currentVoltage*/delta)
             totalCharge += it.totalCharge
-            totalPower += it.totalPower
+//            totalPower += it.totalPower
         }
         Logger.recordOutput("PowerTracker/Robot Total Charge", totalCharge, "amp seconds")
-        Logger.recordOutput("PowerTracker/Robot Energy Usage", totalPower, "watt hours")
+//        Logger.recordOutput("PowerTracker/Robot Energy Usage", totalPower, "watt hours")
     }
 }
 
 private class MotorTracker(val name: String, val currentSupplier: () -> Double, val voltageSupplier: () -> Double? = {null}, val numMotors: Int = 1) {
     var totalCharge = 0.0
-    var totalPower = 0.0
+//    var totalPower = 0.0
 
-    fun update(currentVoltage: Double) {
+    fun update(/*currentVoltage: Double,*/ delta: Double = 0.02) {
         val current = numMotors * currentSupplier.invoke()
-        val power = (voltageSupplier.invoke() ?: currentVoltage) * numMotors * currentSupplier.invoke()
+//        val power = (voltageSupplier.invoke() ?: currentVoltage) * numMotors * currentSupplier.invoke()
 
-        Logger.recordOutput("PowerTracker/${name} Supply Voltage", voltageSupplier.invoke() ?: 0.0, "volts")
+//        Logger.recordOutput("PowerTracker/${name} Supply Voltage", voltageSupplier.invoke() ?: 0.0, "volts")
 
         Logger.recordOutput("PowerTracker/${name} Current Draw", current, "amps")
-        Logger.recordOutput("PowerTracker/${name} Power Draw", power, "watts")
+//        Logger.recordOutput("PowerTracker/${name} Power Draw", power, "watts")
 
-        totalCharge += current / 50.0
-        totalPower += power / 50.0
+        totalCharge += current * delta
+//        totalPower += power * delta
 
         Logger.recordOutput("PowerTracker/${name} Total Charge", totalCharge, "amp seconds")
-        Logger.recordOutput("PowerTracker/${name} Energy Usage", totalPower, "watt hours")
+//        Logger.recordOutput("PowerTracker/${name} Energy Usage", totalPower, "watt hours")
     }
 }
