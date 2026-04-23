6d9a4fdf7ba8dc0212a99b6f514a1d2ba9dc2fcc
Justin Kilk <justin@kilk.com>
2026-04-23 13:42:38 -0700

    More documentation on command composition

--- src/main/kotlin/org/team2471/frc/lib/commands/CommandUtils.kt
+++ src/main/kotlin/org/team2471/frc/lib/commands/CommandUtils.kt
@@ -8,6 +8,7 @@ import org.team2471.frc.lib.units.seconds
 import org.wpilib.commands3.Command
 import org.wpilib.commands3.Coroutine
 import org.wpilib.commands3.Mechanism
+import org.wpilib.commands3.NeedsExecutionBuilderStage
 import org.wpilib.commands3.NeedsNameBuilderStage
 
 class PeriodicScope {
@@ -20,33 +21,81 @@ class PeriodicScope {
 }
 
 
-// Command Composers
-fun use(name: String, vararg mechanisms: Mechanism, body: Coroutine.() -> Unit): Command = Command.requiring(setOf(*mechanisms)).executing(body).named(name)
-fun use(name: String, vararg mechanisms: Mechanism, body: Coroutine.() -> Unit, onCancel: () -> Unit): Command = Command.requiring(setOf(*mechanisms)).executing(body).whenCanceled(onCancel).named(name)
-fun use(vararg mechanisms: Mechanism, body: Coroutine.() -> Unit): NeedsNameBuilderStage = Command.requiring(setOf(*mechanisms)).executing(body)
-fun (Coroutine.() -> Unit).use(name: String, vararg mechanisms: Mechanism, onCancel: () -> Unit = {}) = use(name, *mechanisms, body = this, onCancel = onCancel)
+
+// Partial Command Composers
+/**
+ * Partially composes a nameless command with [body] using provided [Mechanism]s.
+ * This partial constructor is so the partial command can be modified before being composed/named, such as with [NeedsNameBuilderStage.whenCanceled]
+ *
+ * @param mechanisms required mechanisms for the command to execute.
+ * @param body the executed [Coroutine] action.
+ * @return a [NeedsNameBuilderStage] that must be [named] to be a valid [Command].
+ * @see Command.requiring
+ * @see NeedsExecutionBuilderStage.executing
+ */
+fun use(vararg mechanisms: Mechanism, body: Coroutine.() -> Unit): NeedsNameBuilderStage =
+    Command.requiring(setOf(*mechanisms)).executing(body)
+
+// Full Command Composers
+/**
+ * Composes a [Command] with a [body] using the provided [Mechanism]s
+ *
+ * @param name name of the command.
+ * @param mechanisms required mechanisms for the command to execute.
+ * @param body the executed [Coroutine] action.
+ * @param onCancel the function to run when the command gets canceled, doesn't run if the command completes naturally.
+ * @see Command.requiring
+ * @see NeedsExecutionBuilderStage.executing
+ * @see NeedsNameBuilderStage.named
+ */
+fun use(name: String, vararg mechanisms: Mechanism, body: Coroutine.() -> Unit, onCancel: () -> Unit): Command =
+    use(*mechanisms, body = body).named(name, onCancel)
+/**
+ * Composes a [Command] with an action using provided [Mechanism]s
+ *
+ * @param name the name of the command.
+ * @param mechanisms required mechanisms for the command to execute.
+ * @param body the executed [Coroutine] action.
+ * @see Command.requiring
+ * @see NeedsExecutionBuilderStage.executing
+ * @see NeedsNameBuilderStage.named
+ */
+fun use(name: String, vararg mechanisms: Mechanism, body: Coroutine.() -> Unit): Command =
+    use(*mechanisms, body = body).named(name)
+/**
+ * Composes a [Command] with an action using provided [Mechanism]s from a [Coroutine] function
+ *
+ * @param name the name of the command.
+ * @param mechanisms the mechanisms the command requires.
+ * @param onCancel the function to run when the command gets canceled, doesn't run if the command completes naturally.
+ * @see Command.requiring
+ * @see NeedsExecutionBuilderStage.executing
+ * @see NeedsNameBuilderStage.named
+ */
+fun (Coroutine.() -> Unit).use(name: String, vararg mechanisms: Mechanism, onCancel: () -> Unit = {}) =
+    use(name, *mechanisms, body = this, onCancel = onCancel)
 
 /**
  * Finishes composing a command by naming it. Also, can apply an [onCancel] action.
- * The [onCancel] action will not run if the command completes naturally.
+ * [onCancel] will not run if the command completes naturally.
  *
  * @param name the name of the command.
- * @param onCancel the action to perform when the command is canceled.
- * @return composed command with name and optional onCancel action.
+ * @param onCancel the function to run when the command gets canceled, doesn't run if the command completes naturally.
+ * @return a composed [Command].
  * @see NeedsNameBuilderStage.named
  * @see NeedsNameBuilderStage.whenCanceled
  */
 fun NeedsNameBuilderStage.named(name: String, onCancel: () -> Unit = {}): Command = whenCanceled(onCancel).named(name)
 
 /**
- * Modifies existing command to have an [onCancel] action.
- * The [onCancel] action will not run if the command completes naturally.
+ * Modifies an existing commands' [onCancel] action.
+ * [onCancel] will not run if the command completes naturally.
  *
  * @param onCancel the action to perform when the command is canceled.
  * @return the modified command.
  * @see NeedsNameBuilderStage.whenCanceled
  */
-fun Command.onCanceled(onCancel: () -> Unit): Command = Command.requiring(this.requirements()).executing(this::run).whenCanceled(onCancel).named("${this.name()}WithOnCancel")
+fun Command.modifyOnCancel(onCancel: () -> Unit): Command = Command.requiring(this.requirements()).executing(this::run).whenCanceled(onCancel).named("${this.name()}WithOnCancel")
 
 
 inline fun Coroutine.periodic(
