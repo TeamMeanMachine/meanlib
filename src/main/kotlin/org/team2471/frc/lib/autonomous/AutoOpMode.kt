6154034f0b4163fb555a7ab6b121fde2c53238b4
Justin Kilk <justin@kilk.com>
2026-06-24 21:05:01 -0700

    Massively simplify AutoOpMode and TestOpMode by using a Trigger bounded to enable. Updated wpilib to the latest dev branch, must use custom graderio version for it to compile

--- src/main/kotlin/org/team2471/frc/lib/autonomous/AutoOpMode.kt
+++ src/main/kotlin/org/team2471/frc/lib/autonomous/AutoOpMode.kt
@@ -1,11 +1,12 @@
 package org.team2471.frc.lib.autonomous
 
 import org.wpilib.command3.Command
-import org.wpilib.command3.Scheduler
-import org.wpilib.opmode.PeriodicOpMode
+import org.wpilib.command3.Trigger
+import org.wpilib.driverstation.RobotState
+import org.wpilib.opmode.OpMode
 
 /**
- * Autonomous [org.wpilib.opmode.OpMode].
+ * Autonomous [OpMode].
  *
  * When selected will call the [warmupFunction] provided by [Autonomi.warmupFunction]
  *
@@ -13,30 +14,19 @@ import org.wpilib.opmode.PeriodicOpMode
  *
  * Schedules [autoCommand] when enabled and runs it until its disabled or finished.
  */
-class AutoOpMode(val name: String, val autoCommand: Command, warmupFunction: () -> Unit, val disabledPeriodicFunction: () -> Unit): PeriodicOpMode() {
-    val scheduler = Scheduler.getDefault()
+class AutoOpMode(name: String, autoCommand: Command, warmupFunction: () -> Unit, val disabledPeriodicFunction: () -> Unit): OpMode {
 
     init {
         println("$name auto created")
         warmupFunction()
         println("$name auto warmup complete.")
+        // Binds a trigger to the enabled state of the robot.
+        // Acts as if "enabled" is a joystick button. When true, it will schedule the command, when false, it will cancel it.
+        // This Trigger also makes use of scoping. When the AutoOpMode is no longer selected, this trigger will go away.
+        Trigger(RobotState::isEnabled).whileTrue(autoCommand)
     }
 
     override fun disabledPeriodic() {
         disabledPeriodicFunction()
-    }
-
-    override fun start() {
-        println("scheduling $name auto")
-        scheduler.schedule(autoCommand)
-        println("Scheduled auto")
-    }
-
-    override fun periodic() {}
-
-    override fun end() {
-        println("ending $name auto")
-        scheduler.cancel(autoCommand)
-        println("Cancelled auto")
     }
 }
--- src/main/kotlin/org/team2471/frc/lib/autonomous/TestOpMode.kt
+++ src/main/kotlin/org/team2471/frc/lib/autonomous/TestOpMode.kt
@@ -1,33 +1,24 @@
 package org.team2471.frc.lib.autonomous
 
 import org.wpilib.command3.Command
-import org.wpilib.command3.Scheduler
-import org.wpilib.opmode.PeriodicOpMode
+import org.wpilib.command3.Trigger
+import org.wpilib.driverstation.RobotState
+import org.wpilib.opmode.OpMode
 
 /**
- * Test OpMode.
+ * Test [OpMode].
+ *
+ * When selected will call the [initFunction]
+ *
+ * Schedules [testCommand] when enabled and runs it until its disabled or finished.
  */
-class TestOpMode(val name: String, val testCommand: Command, initFunction: () -> Unit): PeriodicOpMode() {
-    val scheduler = Scheduler.getDefault()
-
+class TestOpMode(val name: String, testCommand: Command, initFunction: () -> Unit): OpMode {
     init {
         println("$name test created")
         initFunction()
+        // Binds a trigger to the enabled state of the robot.
+        // Acts as if "enabled" is a joystick button. When true, it will schedule the command, when false, it will cancel it.
+        // This Trigger also makes use of scoping. When the TestOpMode is no longer selected, this trigger will go away.
+        Trigger(RobotState::isEnabled).whileTrue(testCommand)
     }
-
-    override fun start() {
-        println("scheduling $name test")
-        scheduler.schedule(testCommand)
-        println("Scheduled test")
-    }
-
-    override fun periodic() {}
-
-    override fun end() {
-        println("ending $name test")
-        scheduler.cancel(testCommand)
-        println("Cancelled test")
-    }
-
-
 }
